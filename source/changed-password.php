10b9509aceb739a46e4958a8a8eb58d3df3665ce
dcakinn <88024837+dcakinn@users.noreply.github.com>
2025-01-06 01:16:23 +0300

    Create changed-password.php

---
+++ source/changed-password.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html xmlns="http://abcd/xhtml" xml:lang="en-US" lang="en-US">
+<head>
+      <meta http-equiv='Content-Type' content='text/html; charset=utf-8'/>
+      <title>Changed password</title>
+      <link rel="STYLESHEET" type="text/css" href="style/membersite.css">
+</head>
+<body>
+<div id='membersite_content'>
+<h2>Changed password</h2>
+Your password is updated!
+
+<p>
+<a href='logout.php'>logout</a>
+</p>
+
+</div>
+</body>
+</html>
