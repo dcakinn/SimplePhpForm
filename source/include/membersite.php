92a3fb86e82100aac3cfcaa2a5cbe2875059ffec
dcakinn <88024837+dcakinn@users.noreply.github.com>
2025-01-05 23:23:03 +0300

    Create membersite.php

---
+++ source/include/membersite.php
@@ -0,0 +1,490 @@
+<?PHP
+
+require_once("class.phpmailer.php");
+require_once("formvalidator.php");
+
+class Membersite
+{
+    var $admin_email;
+    var $from_address;
+    
+    var $username;
+    var $pass;
+    var $database;
+    var $tablename;
+    var $connection;
+    var $rand_key;
+    
+    var $error_message;
+    
+    //-----Initialization -------
+    function Membersite()
+    {
+        $this->sitename = 'YourWebsiteName.com';
+        $this->rand_key = '0iQx5oBk66oVZep';
+    }
+    
+    function InitDB($host,$uname,$pwd,$database,$tablename)
+    {
+        $this->db_host  = $host;
+        $this->username = $uname;
+        $this->pwd  = $pwd;
+        $this->database  = $database;
+        $this->tablename = $tablename;
+        
+    }
+    function SetAdminEmail($email)
+    {
+        $this->admin_email = $email;
+    }
+    
+    function SetWebsiteName($sitename)
+    {
+        $this->sitename = $sitename;
+    }
+    
+    function SetRandomKey($key)
+    {
+        $this->rand_key = $key;
+    }
+    
+    //-------Main Operations ----------------------
+    function RegisterUser()
+    {
+        if(!isset($_POST['submitted']))
+        {
+           return false;
+        }
+        
+        $formvars = array();
+        
+        if(!$this->ValidateRegistrationSubmission())
+        {
+            return false;
+        }
+        
+        $this->CollectRegistrationSubmission($formvars);
+        
+        if(!$this->SaveToDatabase($formvars))
+        {
+            return false;
+        }
+        
+        if(!$this->SendUserConfirmationEmail($formvars))
+        {
+            return false;
+        }
+
+        $this->SendAdminIntimationEmail($formvars);
+        
+        return true;
+    }
+
+    function ConfirmUser()
+    {
+        if(empty($_GET['code'])||strlen($_GET['code'])<=10)
+        {
+            $this->HandleError("Please provide the confirm code");
+            return false;
+        }
+        $user_rec = array();
+        if(!$this->UpdateDBRecForConfirmation($user_rec))
+        {
+            return false;
+        }
+        
+        $this->SendUserWelcomeEmail($user_rec);
+        
+        $this->SendAdminIntimationOnRegComplete($user_rec);
+        
+        return true;
+    }    
+    
+    function Login()
+    {
+        if(empty($_POST['username']))
+        {
+            $this->HandleError("UserName is empty!");
+            return false;
+        }
+        
+        if(empty($_POST['password']))
+        {
+            $this->HandleError("Password is empty!");
+            return false;
+        }
+        
+        $username = trim($_POST['username']);
+        $password = trim($_POST['password']);
+        
+        if(!isset($_SESSION)){ session_start(); }
+        if(!$this->CheckLoginInDB($username,$password))
+        {
+            return false;
+        }
+        
+        $_SESSION[$this->GetLoginSessionVar()] = $username;
+        
+        return true;
+    }
+    
+    function CheckLogin()
+    {
+         if(!isset($_SESSION)){ session_start(); }
+
+         $sessionvar = $this->GetLoginSessionVar();
+         
+         if(empty($_SESSION[$sessionvar]))
+         {
+            return false;
+         }
+         return true;
+    }
+    
+    function UserName()
+    {
+        return isset($_SESSION['name_of_user'])?$_SESSION['name_of_user']:'';
+    }
+    
+    function UserEmail()
+    {
+        return isset($_SESSION['email_of_user'])?$_SESSION['email_of_user']:'';
+    }
+    
+    function LogOut()
+    {
+        session_start();
+        
+        $sessionvar = $this->GetLoginSessionVar();
+        
+        $_SESSION[$sessionvar]=NULL;
+        
+        unset($_SESSION[$sessionvar]);
+    }
+    
+    function EmailResetPasswordLink()
+    {
+        if(empty($_POST['email']))
+        {
+            $this->HandleError("Email is empty!");
+            return false;
+        }
+        $user_rec = array();
+        if(false === $this->GetUserFromEmail($_POST['email'], $user_rec))
+        {
+            return false;
+        }
+        if(false === $this->SendResetPasswordLink($user_rec))
+        {
+            return false;
+        }
+        return true;
+    }
+    
+    function ResetPassword()
+    {
+        if(empty($_GET['email']))
+        {
+            $this->HandleError("Email is empty!");
+            return false;
+        }
+        if(empty($_GET['code']))
+        {
+            $this->HandleError("reset code is empty!");
+            return false;
+        }
+        $email = trim($_GET['email']);
+        $code = trim($_GET['code']);
+        
+        if($this->GetResetPasswordCode($email) != $code)
+        {
+            $this->HandleError("Bad reset code!");
+            return false;
+        }
+        
+        $user_rec = array();
+        if(!$this->GetUserFromEmail($email,$user_rec))
+        {
+            return false;
+        }
+        
+        $new_password = $this->ResetUserPasswordInDB($user_rec);
+        if(false === $new_password || empty($new_password))
+        {
+            $this->HandleError("Error updating new password");
+            return false;
+        }
+        
+        if(false == $this->SendNewPassword($user_rec,$new_password))
+        {
+            $this->HandleError("Error sending new password");
+            return false;
+        }
+        return true;
+    }
+    
+    function ChangePassword()
+    {
+        if(!$this->CheckLogin())
+        {
+            $this->HandleError("Not logged in!");
+            return false;
+        }
+        
+        if(empty($_POST['oldpass']))
+        {
+            $this->HandleError("Old password is empty!");
+            return false;
+        }
+        if(empty($_POST['newpass']))
+        {
+            $this->HandleError("New password is empty!");
+            return false;
+        }
+        
+        $user_rec = array();
+        if(!$this->GetUserFromEmail($this->UserEmail(),$user_rec))
+        {
+            return false;
+        }
+        
+        $pwd = trim($_POST['oldpass']);
+        
+        if(!password_verify($pass, $user_rec['password']))
+        {
+            $this->HandleError("The old password does not match!");
+            return false;
+        }
+        $newpwd = trim($_POST['newpass']);
+        
+        if(!$this->ChangePasswordInDB($user_rec, $newpass))
+        {
+            return false;
+        }
+        return true;
+    }
+    
+    
+    function GetFromAddress()
+    {
+        if(!empty($this->from_address))
+        {
+            return $this->from_address;
+        }
+
+        $host = $_SERVER['SERVER_NAME'];
+
+        $from ="nobody@$host";
+        return $from;
+    } 
+    
+    function GetLoginSessionVar()
+    {
+        $retvar = md5($this->rand_key);
+        $retvar = 'usr_'.substr($retvar,0,10);
+        return $retvar;
+    }
+    
+    function CheckLoginInDB($username,$password)
+    {
+        if(!$this->DBLogin())
+        {
+            $this->HandleError("Database login failed!");
+            return false;
+        }          
+        $username = $this->SanitizeForSQL($username);
+
+        $nresult = mysqli_query($this->connection, 
+          "SELECT * FROM $this->tablename WHERE username = '$username'") 
+              or die(mysqli_error($this->connection));
+          
+        // check for result 
+        $no_of_rows = mysqli_num_rows($nresult);
+        
+        if($no_of_rows <= 0)
+        {
+            $this->HandleError("Error logging in. The username or password does not match");
+            return false;
+        }
+        
+        $row = mysqli_fetch_assoc($nresult);
+        if($row['confirmcode']!= 'y')
+        {
+            $this->HandleError("Error logging in. The username or password does not match");
+            return false;            
+        }
+        
+        if(!password_verify($password , $row['password'] ))
+        {
+            $this->HandleError("Error logging in. The username or password does not match");
+            return false;
+        }
+
+        $_SESSION['name_of_user']  = $row['name'];
+        $_SESSION['email_of_user'] = $row['email'];
+        
+        return true;
+    }
+   
+    
+    function ResetUserPasswordInDB($user_rec)
+    {
+        $new_password = substr(md5(uniqid()),0,10);
+        
+        if(false == $this->ChangePasswordInDB($user_rec,$new_password))
+        {
+            return false;
+        }
+        return $new_password;
+    }
+    
+    function ChangePasswordInDB($user_rec, $newpwd)
+    {
+        $newpwd = trim($newpass);
+        $new_password = password_hash($newpass, PASSWORD_DEFAULT);
+        
+        $qry = "Update $this->tablename Set password='".$new_password."' Where  id_user=".$user_rec['id_user']."";
+        
+        if(!mysqli_query($this->connection, $qry))
+        {
+            $this->HandleDBError("Error updating the password \nquery:$qry");
+            return false;
+        }     
+        return true;
+    }
+    
+    function GetUserFromEmail($email,&$user_rec)
+    {
+        if(!$this->DBLogin())
+        {
+            $this->HandleError("Database login failed!");
+            return false;
+        }   
+        $email = $this->SanitizeForSQL($email);
+        
+        $result = mysqli_query($this->connection, "Select * from $this->tablename where email='$email'");  
+
+        if(!$result || mysqli_num_rows($result) <= 0)
+        {
+            $this->HandleError("There is no user with email: $email");
+            return false;
+        }
+        $user_rec = mysqli_fetch_assoc($result);
+
+        
+        return true;
+    }
+    
+    
+    function GetResetPasswordCode($email)
+    {
+       return substr(md5($email.$this->sitename.$this->rand_key),0,10);
+    }
+    
+    function SendResetPasswordLink($user_rec)
+    {
+        $email = $user_rec['email'];
+        
+        $mailer = new PHPMailer();
+        
+        $mailer->CharSet = 'utf-8';
+        
+        $mailer->AddAddress($email,$user_rec['name']);
+        
+        $mailer->Subject = "Your reset password request at ".$this->sitename;
+
+        $mailer->From = $this->GetFromAddress();
+        
+        $link = $this->GetAbsoluteURLFolder().
+                '/resetpass.php?email='.
+                urlencode($email).'&code='.
+                urlencode($this->GetResetPasswordCode($email));
+
+        $mailer->Body ="Hello ".$user_rec['name']."\r\n\r\n".
+        "There was a request to reset your password at ".$this->sitename."\r\n".
+        "Please click the link below to complete the request: \r\n".$link."\r\n".
+        "Regards,\r\n".
+        "Webmaster\r\n".
+        $this->sitename;
+        
+        if(!$mailer->Send())
+        {
+            return false;
+        }
+        return true;
+    }
+    
+   
+    function ValidateRegistrationSubmission()
+    {
+        if(!empty($_POST[$this->GetSpamTrapInputName()]) )
+        {
+            //The proper error is not given intentionally
+            $this->HandleError("Automated submission prevention: case 2 failed");
+            return false;
+        }
+        
+        $validator = new FormValidator();
+        $validator->addValidation("name","req","Please fill in Name");
+        $validator->addValidation("email","email","The input for Email should be a valid email value");
+        $validator->addValidation("email","req","Please fill in Email");
+  
+        $validator->addValidation("password","req","Please fill in Password");
+
+        
+        if(!$validator->ValidateForm())
+        {
+            $error='';
+            $error_hash = $validator->GetErrors();
+            foreach($error_hash as $inpname => $inp_err)
+            {
+                $error .= $inpname.':'.$inp_err."\n";
+            }
+            $this->HandleError($error);
+            return false;
+        }        
+        return true;
+    }
+    
+    function CollectRegistrationSubmission(&$formvars)
+    {
+        $formvars['name'] = $this->Sanitize($_POST['name']);
+	$formvars['username'] = $this->Sanitize($_POST['username']);
+        $formvars['email'] = $this->Sanitize($_POST['email']);
+        $formvars['password'] = $this->Sanitize($_POST['password']);
+   
+    }
+    
+    function SendUserConfirmationEmail(&$formvars)
+    {
+        $mailer = new PHPMailer();
+        
+        $mailer->CharSet = 'utf-8';
+        
+        $mailer->AddAddress($formvars['email'],$formvars['name']);
+        
+        $mailer->Subject = "Your registration with ".$this->sitename;
+
+        $mailer->From = $this->GetFromAddress();        
+        
+        $confirmcode = $formvars['confirmcode'];
+        
+        $confirm_url = $this->GetAbsoluteURLFolder().'/confirmreg.php?code='.$confirmcode;
+        
+        $mailer->Body ="Hello ".$formvars['name']."\r\n\r\n".
+        "Thanks for your registration with ".$this->sitename."\r\n".
+        "Please click the link below to confirm your registration.\r\n".
+        "$confirm_url\r\n".
+        "\r\n".
+        "Regards,\r\n".
+        "Webmaster\r\n".
+        $this->sitename;
+
+        if(!$mailer->Send())
+        {
+            $this->HandleError("Failed sending registration confirmation email.");
+            return false;
+        }
+        return true;
+    }
+    
