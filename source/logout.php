55ba5c08e55c2529d64e741f62378bc37a45dc9c
dcakinn <88024837+dcakinn@users.noreply.github.com>
2025-01-05 23:35:45 +0300

    Create logout.php

---
+++ source/logout.php
@@ -0,0 +1,22 @@
+<?PHP
+require_once("./include/membersite_config.php");
+
+$membersite->LogOut();
+?>
+<!DOCTYPE html >
+<html xmlns="http://abcdlogout/xhtml" xml:lang="en-US" lang="en-US">
+<head>
+      <meta http-equiv='Content-Type' content='text/html; charset=utf-8'/>
+      <title>Login</title>
+      <link rel="STYLESHEET" type="text/css" href="style/fg_membersite.css" />
+      <script type='text/javascript' src='scripts/gen_validatorv31.js'></script>
+</head>
+<body>
+
+<h2>You have logged out. Please login back to continue.</h2>
+<p>
+<a href='login.php'>Login</a>
+</p>
+
+</body>
+</html>
