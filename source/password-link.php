e3c084b31ca040fc9213b89802ca99c933add6ec
dcakinn <88024837+dcakinn@users.noreply.github.com>
2025-01-05 23:57:02 +0300

    Create password-link.php

---
+++ source/password-link.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html">
+<html xmlns="http://abcd/xhtml" xml:lang="en-US" lang="en-US">
+<head>
+      <meta http-equiv='Content-Type' content='text/html; charset=utf-8'/>
+      <title>Thank you!</title>
+      <link rel="STYLESHEET" type="text/css" href="style/fg_membersite.css">
+</head>
+<body>
+<div id='fg_membersite_content'>
+<h2>Reset password link successfully sent.</h2>
+An email is sent to your email address that contains the link to reset the password. You can change your password by clicking it.
+</div>
+</body>
+</html>
